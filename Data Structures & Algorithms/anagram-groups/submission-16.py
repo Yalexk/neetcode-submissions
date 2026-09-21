ae5e4a3da3e1803199d803b3615e992cba3ab21d
Alex Yecies <33238564+Yalexk@users.noreply.github.com>
2026-09-21 21:04:15 +0200

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.py
@@ -0,0 +1,24 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # go through each string
+        # create an array for each string
+        # array to index
+        ans = []
+        hmap = {}
+
+        for s in strs:
+            s_arr = [0] * 26
+            for c in s:
+                # print(ord(c))
+                s_arr[ord(c) - 97] += 1
+
+            s_arr = tuple(s_arr)
+            if s_arr not in hmap:
+                hmap[s_arr] = [s]
+            else:
+                hmap[s_arr].append(s)
+
+        return list(hmap.values())        
+            
+
+        
