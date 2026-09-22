6f90a5d7a892926c993dba218bd4d924f3285bc4
Alex Yecies <33238564+Yalexk@users.noreply.github.com>
2026-09-22 16:03:31 +0200

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hmap = {}
+        freqs = [[] for _ in range(len(nums) + 1)] # map frequency to array index
+        for num in nums:
+            count = hmap.get(num)
+            if not count:
+                hmap[num] = 1
+                freqs[1].append(num)
+
+            else:
+                hmap[num] += 1
+                freqs[hmap[num]].append(num)
+        
+        # print(hmap)
+        # print(freqs)
+        seen = []
+        ans = []
+        i = len(nums)
+        while i > 0 and k > 0:
+            for num in freqs[i]:
+                if num not in seen:
+                    seen.append(num)
+                    k -= 1
+                    ans.append(num)
+            i -= 1
+        
+        return list(ans)
